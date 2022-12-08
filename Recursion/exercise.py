cd27838428edb67effbee61d3d42990c132487fd
naimulnobel <nobelnaimulislam@gmail.com>
2022-12-07 22:17:02 -0500

    febonaccy

--- Recursion/exercise.py
+++ Recursion/exercise.py
@@ -8,7 +8,8 @@
 
 # recursion(5)
 def factorial(n):
-    if n == 1:
+    assert n >= 0 and int(n) == n, "The number must be positive integer only!"
+    if n in [0, 1]:
         return 1
     else:
         return n * factorial(n-1)
