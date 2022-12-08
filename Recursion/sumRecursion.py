a0226ae0b3fc8143d30c109406faeac35aeefe0a
naimulnobel <nobelnaimulislam@gmail.com>
2022-12-08 18:25:43 -0500

    power

---
+++ Recursion/sumRecursion.py
@@ -0,0 +1,6 @@
+def sumOfDigits(n):
+    assert n >= 0 and int(n) == n, "The number must be positive integer only!"
+    if n == 0:
+        return 0
+    else:
+        return int(n % 10) + sumOfDigits(int(n/10))
